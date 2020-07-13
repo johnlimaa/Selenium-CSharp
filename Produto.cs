051d6cee19c189a99b0d1455c0985271c02e9c09
Jhonatta Lima <47035885+johnlimaa@users.noreply.github.com>
2020-07-13 15:35:20 -0300

    Arquivos iniciais

---
+++ Produto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Text;
+
+namespace ScrapingATip
+{
+	class Produto
+	{
+		public string nome { get; set; }
+		public string descricao { get; set; }
+		public string menorValor { get; set; }
+		public string maiorValor { get; set; }
+	}
+}
---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using Newtonsoft.Json;
+
+namespace ScrapingATip
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			IWebDriver driver = new ChromeDriver();
+			driver.Navigate().GoToUrl("https://www.boadica.com.br/produtos/p170681/");
+
+			Produto produto = new Produto();
+
+			produto.nome = driver.FindElement(By.ClassName("nome")).Text.ToString();
+			produto.descricao = driver.FindElement(By.ClassName("especificacao")).Text.ToString();
+			produto.menorValor = driver
+				.FindElement(By.XPath("/html/body/div[2]/div[2]/div[1]/div/div/div[1]/div[2]/div[2]/div/div[2]/div/div/div[1]/span[1]"))
+				.Text
+				.ToString();
+			produto.maiorValor = driver
+				.FindElement(By.XPath("/html/body/div[2]/div[2]/div[1]/div/div/div[1]/div[2]/div[2]/div/div[2]/div/div/div[1]/span[2]"))
+				.Text
+				.ToString();
+
+			string produtoConvertidoJson = JsonConvert.SerializeObject(produto);
+
+			Console.WriteLine(produtoConvertidoJson);
+		}
+	}
+}
